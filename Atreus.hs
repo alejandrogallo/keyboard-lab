75c787c7a02e575105d8304ec6ac1cfaf3294ac8
Alejandro Gallo <aamsgallo@gmail.com>
2020-09-21 02:25:01 +0200

    Add ortho atreus

--- Atreus.hs
+++ Atreus.hs
@@ -3,36 +3,37 @@ module Atreus where
 import Keyboard
 
 
-atreus :: Keyboard
-atreus = [ Key 0.0 (0.0 * sepY) "כ"
-         , Key 0.0 (1.0 * sepY) "Z"
-         , Key 0.0 (2.0 * sepY) "A"
-         , Key 0.0 (3.0 * sepY) "Q"
+parametrizedAtreus :: Double -> Keyboard
+parametrizedAtreus offY1
+  = [ Key 0.0 (0.0 * sepY) "כ"
+    , Key 0.0 (1.0 * sepY) "Z"
+    , Key 0.0 (2.0 * sepY) "A"
+    , Key 0.0 (3.0 * sepY) "Q"
 
-         , Key sepX1 (0.0 * sepY + offY1) "א"
-         , Key sepX1 (1.0 * sepY + offY1) "X"
-         , Key sepX1 (2.0 * sepY + offY1) "S"
-         , Key sepX1 (3.0 * sepY + offY1) "W"
+    , Key sepX1 (0.0 * sepY + offY1) "א"
+    , Key sepX1 (1.0 * sepY + offY1) "X"
+    , Key sepX1 (2.0 * sepY + offY1) "S"
+    , Key sepX1 (3.0 * sepY + offY1) "W"
 
-         , Key sepX2 (0.0 * sepY + offY2) "ב"
-         , Key sepX2 (1.0 * sepY + offY2) "C"
-         , Key sepX2 (2.0 * sepY + offY2) "D"
-         , Key sepX2 (3.0 * sepY + offY2) "E"
+    , Key sepX2 (0.0 * sepY + offY2) "ב"
+    , Key sepX2 (1.0 * sepY + offY2) "C"
+    , Key sepX2 (2.0 * sepY + offY2) "D"
+    , Key sepX2 (3.0 * sepY + offY2) "E"
 
-         , Key sepX3 (0.0 * sepY + offY3) "ד"
-         , Key sepX3 (1.0 * sepY + offY3) "V"
-         , Key sepX3 (2.0 * sepY + offY3) "F"
-         , Key sepX3 (3.0 * sepY + offY3) "R"
+    , Key sepX3 (0.0 * sepY + offY3) "ד"
+    , Key sepX3 (1.0 * sepY + offY3) "V"
+    , Key sepX3 (2.0 * sepY + offY3) "F"
+    , Key sepX3 (3.0 * sepY + offY3) "R"
 
-         , Key sepX4 (0.0 * sepY + offY4) "ל"
-         , Key sepX4 (1.0 * sepY + offY4) "B"
-         , Key sepX4 (2.0 * sepY + offY4) "G"
-         , Key sepX4 (3.0 * sepY + offY4) "T"
+    , Key sepX4 (0.0 * sepY + offY4) "ל"
+    , Key sepX4 (1.0 * sepY + offY4) "B"
+    , Key sepX4 (2.0 * sepY + offY4) "G"
+    , Key sepX4 (3.0 * sepY + offY4) "T"
 
-         , Key sepX5 (0.0 * sepY + offY5) "ל"
-         , Key sepX5 (1.0 * sepY + offY5) "ל"
+    , Key sepX5 (0.0 * sepY + offY5) "ל"
+    , Key sepX5 (1.0 * sepY + offY5) "ל"
 
-         ]
+    ]
 
     where sepY = 1.95
           sepX1 = 2
@@ -40,8 +41,13 @@ atreus = [ Key 0.0 (0.0 * sepY) "כ"
           sepX3 = 3 * sepX1
           sepX4 = 4 * sepX1
           sepX5 = 5 * sepX1
-          offY1 = 0.4
           offY2 = 2 * offY1
           offY3 = 1 * offY1
           offY4 = 0 * offY1
           offY5 = (-0.8) * offY1
+
+atreus :: Keyboard
+atreus = parametrizedAtreus 0.4
+
+orthoAtreus :: Keyboard
+orthoAtreus = parametrizedAtreus 0.0
