a34dbb54a03fce01d26dde5e33341309343c5b4e
Alejandro Gallo <aamsgallo@gmail.com>
2020-09-28 13:31:28 +0200

    Update Promicro

--- Promicro.hs
+++ Promicro.hs
@@ -4,12 +4,16 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 promicro :: Diagram B
-promicro = mconcat $ [ rect w h ] ++ pinsL ++ pinsR ++ [label]
-  where r = 0.1
-        pt = circle (r/2) # lc green
-        w = 1.8
-        h = 3.3
-        angle = 90 @@ deg
-        label = text "promicro" # rotate angle # scale 0.5
-        pinsR = [ place pt $ p2 (w/2 - r, y * 0.25 - h / 2 + r) | y <- [0..11]]
-        pinsL = [ place pt $ p2 (r - w/2, y * 0.25 - h / 2 + r) | y <- [0..11]]
+promicro = rect w h <> pinsL <> pinsR <> label
+  where
+    r = 0.1
+    pt = circle (r/2) # lc green
+    w = 1.8
+    h = 3.3
+    label = text "promicro" # rotate (90 @@ deg) # scale 0.5
+    pinsR = drawPoints [(w/2 - r, y * 0.25 - h / 2 + r) | y <- [0..11]]
+    pinsL = drawPoints [(r - w/2, y * 0.25 - h / 2 + r) | y <- [0..11]]
+    drawPoints ps = mconcat $ place pt . p2 <$> ps
+
+trrs :: Diagram B
+trrs = rect 1.8 0.6 <> text "trrs" # scale 0.5
